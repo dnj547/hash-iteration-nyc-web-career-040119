9b051668553c84095a018979f12f1c984ac7021c
Danielle Jasper <dnj547@live.com>
2019-03-10 18:13:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,12 +3,10 @@
 #   "Sarah" => 6,
 #   "Amanda" => 27
 # }
-require pry
 def happy_birthday(birthday_kids)
   def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    binding.pry
   end
 end
 end
